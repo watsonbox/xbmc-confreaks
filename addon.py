b7ac4c32e5724ca427d4124eee15450e508ce452
Howard Wilson <howard@watsonbox.net>
2015-03-11 15:01:59 +0100

    Remove video list step since now there is only one per presentation

--- addon.py
+++ addon.py
@@ -13,38 +13,18 @@ plugin = Plugin(PLUGIN_NAME, PLUGIN_ID, __file__)
 def index():
   return [{
     'label': event.name() + '  [COLOR mediumslateblue]' + event.pretty_start() + ' - ' + event.pretty_end() + '[/COLOR]',
-    'path': plugin.url_for('show_presentations', conference=event.code()),
+    'path': plugin.url_for('show_videos', event_code=event.code()),
     #'icon': ''
   } for event in Router.events()]
 
 
-@plugin.route('/conferences/<conference>/')
-def show_presentations(conference):
+@plugin.route('/events/<event_code>/')
+def show_videos(event_code):
   return [{
     'label': video.title + '  [COLOR mediumslateblue]' + video.presenter_names() + '[/COLOR]',
-    'path': plugin.url_for('show_videos', presentation=video.slug)
-  } for video in Router.videos(conference)]
-
-
-@plugin.route('/presentations/<presentation>/')
-def show_videos(presentation):
-  video = Router.video(presentation)
-
-  if video.host() == 'vimeo':
-    return [{
-      'label': 'Vimeo Video',
-      'path': video.url(),
-      'is_playable': True
-    }]
-  elif video.host() == 'youtube':
-    return [{
-      'label': 'YouTube Video',
-      'path': video.url(),
-      'is_playable': True
-    }]
-
-  # No videos could be found
-  return []
+    'path': video.url(),
+    'is_playable': True
+  } for video in Router.videos(event_code)]
 
 
 if __name__ == '__main__':
--- resources/tests/test_addon.py
+++ resources/tests/test_addon.py
@@ -4,50 +4,39 @@ import sys, os
 import unittest
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
-from addon import plugin, index, show_presentations, show_videos
+from addon import plugin, index, show_videos
 
 class IntegrationTests(unittest.TestCase):
 
-  def test_show_presentations(self):
-    items = show_presentations('arrrrcamp2013')
+  def test_show_youtube_videos(self):
+    items = show_videos('arrrrcamp2013')
 
     # There are 22 presentations for ArrrrCamp 2013
     self.assertEqual(len(items), 22)
 
     # The eighth is 'You gotta try this'
     self.assertEqual(items[8], {
-      'label': u'You gotta try this  [COLOR mediumslateblue]Avdi Grimm[/COLOR]',
-      'path': 'plugin://plugin.video.confreaks/presentations/arrrrcamp2013-you-gotta-try-this/'
-    })
-
-
-  def test_show_youtube_videos(self):
-    items = show_videos('arrrrcamp2013-you-gotta-try-this')
-
-    # 'You gotta try this' has a YouTube video
-    self.assertEqual(len(items), 1)
-    self.assertEqual(items[0], {
       'is_playable': True,
-      'label': 'YouTube Video',
+      'label': u'You gotta try this  [COLOR mediumslateblue]Avdi Grimm[/COLOR]',
       'path': 'plugin://plugin.video.youtube/?action=play_video&videoid=sVd4p6oKeUA'
     })
 
 
   def test_show_vimeo_videos(self):
-    items = show_videos('webrebels2012-application-cache-douchebag')
+    items = show_videos('webrebels2012')
 
     # 'Application Cache Douchebag' has a Vimeo video
-    self.assertEqual(len(items), 1)
+    self.assertTrue(len(items) == 17)
     self.assertEqual(items[0], {
       'is_playable': True,
-      'label': 'Vimeo Video',
-      'path': 'plugin://plugin.video.vimeo/?action=play_video&videoid=43336762'
+      'label': 'The JavaScript Virtual Machine  [COLOR mediumslateblue]Dave Herman[/COLOR]',
+      'path': 'plugin://plugin.video.vimeo/?action=play_video&videoid=43380479'
     })
 
 
   def test_index(self):
     items = index()
-    rubyConf2014 = [i for i in items if i['path'] == "plugin://plugin.video.confreaks/conferences/rubyconf2014/"][0]
+    rubyConf2014 = [i for i in items if i['path'] == "plugin://plugin.video.confreaks/events/rubyconf2014/"][0]
 
     # There should be more than 150 events in total
     self.assertTrue(len(items) > 150)
@@ -55,7 +44,7 @@ class IntegrationTests(unittest.TestCase):
     # Ensure RubyConf 2014 data is correct
     self.assertEqual(rubyConf2014, {
       'label': u'Ruby Conference 2014  [COLOR mediumslateblue]Nov 17 - Nov 19[/COLOR]',
-      'path': 'plugin://plugin.video.confreaks/conferences/rubyconf2014/',
+      'path': 'plugin://plugin.video.confreaks/events/rubyconf2014/',
       #'icon': u'http://s3-us-west-2.amazonaws.com/confreaks-tv3/production/events/logos/000/000/225/rubyconf-website-small-thumb.png?1422307583',
     })
 
