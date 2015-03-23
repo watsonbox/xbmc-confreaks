abc7efb0403d1a262b8a33a82b06c0c58879625a
Howard Wilson <howard@watsonbox.net>
2015-03-23 19:52:42 +0100

    Update to support host and embed_code API changes

--- api/video.py
+++ api/video.py
@@ -4,22 +4,11 @@ class Video(object):
     self.slug = json['slug']
     self.title = json['title']
     self.presenters = json['presenters']
-    self.youtube_code = json['youtube_code']
+    self.host = json['host']
+    self.embed_code = json['embed_code']
 
   def presenter_names(self):
     return ', '.join(map(lambda p: p['first_name'] + ' ' + p['last_name'], self.presenters))
 
-  def host(self):
-    if self.youtube_code.startswith('vimeo'):
-      return 'vimeo'
-    else:
-      return 'youtube'
-
-  def code(self):
-    return self.youtube_code.replace('vimeo|', '')
-
   def url(self):
-    if self.host() == 'vimeo':
-      return 'plugin://plugin.video.vimeo/?action=play_video&videoid=%s' % self.code()
-    else:
-      return 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % self.code()
+    return 'plugin://plugin.video.%s/?action=play_video&videoid=%s' % (self.host, self.embed_code)
